05b3cefb644e8527a50ebb66fa5eb119c0461563
ThanosMa <mastrogiannis.ath@gmail.com>
2022-12-09 11:11:31 +0200

    Add project files.

---
+++ DapperCRUDTutorial/Controllers/SuperHeroController.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Identity.Client;
+
+namespace DapperCRUDTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public SuperHeroController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllSuperHeroes()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection")) ;
+            var heroes = await connection.QueryAsync<SuperHero>("select * from SuperHeroes");
+            return Ok(heroes);
+
+        }
+    }
+}
---
+++ DapperCRUDTutorial/SuperHero.cs
@@ -0,0 +1,11 @@
+﻿namespace DapperCRUDTutorial
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }    
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Location { get; set; }
+    }
+}
