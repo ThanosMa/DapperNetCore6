2789a4a63bfe0c8872ff6927c3ea0744034f58a0
ThanosMa <mastrogiannis.ath@gmail.com>
2022-12-09 11:54:09 +0200

    Full crud, backend only

--- DapperCRUDTutorial/Controllers/SuperHeroController.cs
+++ DapperCRUDTutorial/Controllers/SuperHeroController.cs
@@ -19,10 +19,51 @@ namespace DapperCRUDTutorial.Controllers
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GetAllSuperHeroes()
         {
-            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection")) ;
-            var heroes = await connection.QueryAsync<SuperHero>("select * from SuperHeroes");
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            IEnumerable<SuperHero> heroes = await SelectAllHeroes(connection);
             return Ok(heroes);
+        }
 
+        [HttpGet("{heroId}")]
+        public async Task<ActionResult<List<SuperHero>>> GetHero(int heroId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var hero = await connection.QueryFirstAsync<SuperHero>("select * from SuperHeroes where id=@Id",new {Id = heroId});
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> CreateHero(SuperHero hero)
+        {
+            var query = "INSERT INTO SuperHeroes (name, firstname, lastname,location) VALUES (@Name, @FirstName, @LastName, @Location)";
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync(query,hero);
+            return Ok(await SelectAllHeroes(connection));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>>UpdateHero(SuperHero hero)
+        {
+            var query = "UPDATE SuperHeroes SET name=@Name, firstname = @FirstName, lastname = @LastName, location = @Location WHERE id=@Id";
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync(query, new {Id =hero.Id, Name =hero.Name, FirstName = hero.FirstName, LastName = hero.LastName, Location = hero.Location});
+            return Ok(await SelectAllHeroes(connection));
+        }
+
+        [HttpDelete("{heroId}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int heroId)
+        {
+            var query = "DELETE FROM SuperHeroes WHERE id=@Id";
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync(query, new { Id = heroId});
+            return Ok(await SelectAllHeroes(connection));
+        }
+
+
+
+        private static async Task<IEnumerable<SuperHero>> SelectAllHeroes(SqlConnection connection)
+        {
+            return await connection.QueryAsync<SuperHero>("select * from SuperHeroes");
         }
     }
 }
